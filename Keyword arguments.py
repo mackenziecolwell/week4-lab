2ac750b04a852201960144d5b7046ca7c590d89f
mackenziecolwell <M.Colwell2248@student.leedsbeckett.ac.uk>
2023-10-26 08:33:55 +0100

    added Keyword arguments.py

---
+++ Keyword arguments.py
@@ -0,0 +1,8 @@
+def someFunc(x, y, z):
+    print("x is", x, "\ny is", y, "\nz is", z)
+
+someFunc(z = 7, x = 5, y = 3)
+someFunc(x = 22, z = 1, y = 9)
+
+print("these", "are", "some", "words", sep="\n")
+print("more", "words", "wow", sep="!")
